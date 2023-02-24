ebd43a685f79fae6d21d70821f959083d9073607
slaimmials <77630130+slaimmials@users.noreply.github.com>
2023-02-24 15:53:27 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -6,7 +6,7 @@
 local ScreenGui = Instance.new("ScreenGui")
 local MainFrame = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
-local MainFrame_2 = Instance.new("Frame")
+local MainFrame2 = Instance.new("Frame")
 local UICorner_2 = Instance.new("UICorner")
 local UpperFrame = Instance.new("Frame")
 local UICorner_3 = Instance.new("UICorner")
@@ -25,11 +25,14 @@ local TextBox = Instance.new("TextBox")
 local UICorner_7 = Instance.new("UICorner")
 local UITextSizeConstraint_4 = Instance.new("UITextSizeConstraint")
 local TextLabel_2 = Instance.new("TextLabel")
+local TextButton_3 = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
 
 --Properties:
 
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.ResetOnSpawn = false
 
 MainFrame.Name = "MainFrame"
 MainFrame.Parent = ScreenGui
@@ -41,16 +44,16 @@ MainFrame.Size = UDim2.new(0, 656, 0, 434)
 
 UICorner.Parent = MainFrame
 
-MainFrame_2.Name = "MainFrame"
-MainFrame_2.Parent = MainFrame
-MainFrame_2.BackgroundColor3 = Color3.fromRGB(0, 90, 118)
-MainFrame_2.Position = UDim2.new(0.0213580709, 0, 0.0798427016, 0)
-MainFrame_2.Size = UDim2.new(0, 629, 0, 386)
+MainFrame2.Name = "MainFrame2"
+MainFrame2.Parent = MainFrame
+MainFrame2.BackgroundColor3 = Color3.fromRGB(0, 90, 118)
+MainFrame2.Position = UDim2.new(0.0213580709, 0, 0.0798427016, 0)
+MainFrame2.Size = UDim2.new(0, 629, 0, 386)
 
-UICorner_2.Parent = MainFrame_2
+UICorner_2.Parent = MainFrame2
 
 UpperFrame.Name = "UpperFrame"
-UpperFrame.Parent = MainFrame_2
+UpperFrame.Parent = MainFrame2
 UpperFrame.BackgroundColor3 = Color3.fromRGB(0, 130, 177)
 UpperFrame.Position = UDim2.new(0.0111287758, 0, 0.0155440411, 0)
 UpperFrame.Size = UDim2.new(0, 616, 0, 42)
@@ -75,7 +78,7 @@ UITextSizeConstraint.Parent = TextButton
 UITextSizeConstraint.MaxTextSize = 24
 
 Exploit.Name = "Exploit"
-Exploit.Parent = MainFrame_2
+Exploit.Parent = MainFrame2
 Exploit.Active = true
 Exploit.BackgroundColor3 = Color3.fromRGB(0, 90, 118)
 Exploit.BorderColor3 = Color3.fromRGB(0, 0, 0)
@@ -148,29 +151,41 @@ TextLabel_2.Parent = MainFrame
 TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_2.BackgroundTransparency = 1.000
 TextLabel_2.BorderSizePixel = 0
-TextLabel_2.Position = UDim2.new(0.0312775224, 0, 0.0145241022, 0)
-TextLabel_2.Size = UDim2.new(0, 622, 0, 28)
+TextLabel_2.Position = UDim2.new(0.0312775411, 0, 0.00691244239, 0)
+TextLabel_2.Size = UDim2.new(0, 567, 0, 31)
 TextLabel_2.Font = Enum.Font.FredokaOne
-TextLabel_2.Text = "BrookHaven Hub                                                    made by slaimmials"
+TextLabel_2.Text = "BrookHaven Hub                                                 made by slaimmials"
 TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_2.TextSize = 23.000
 TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
 
+TextButton_3.Parent = MainFrame
+TextButton_3.BackgroundColor3 = Color3.fromRGB(0, 90, 118)
+TextButton_3.BorderSizePixel = 0
+TextButton_3.Position = UDim2.new(0.945138633, 0, 0.00691244239, 0)
+TextButton_3.Size = UDim2.new(0, 29, 0, 28)
+TextButton_3.Font = Enum.Font.SourceSans
+TextButton_3.Text = "X"
+TextButton_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_3.TextSize = 14.000
+
+UICorner_8.Parent = TextButton_3
+
 -- Scripts:
 
-local function LMQQ_fake_script() -- TextButton.LocalScript 
+local function CGIGWIZ_fake_script() -- TextButton.LocalScript 
 	local script = Instance.new('LocalScript', TextButton)
 
 	script.Parent.Font = Enum.Font.Sarpanch
 end
-coroutine.wrap(LMQQ_fake_script)()
-local function JGFVBDT_fake_script() -- TextLabel.LocalScript 
+coroutine.wrap(CGIGWIZ_fake_script)()
+local function IILC_fake_script() -- TextLabel.LocalScript 
 	local script = Instance.new('LocalScript', TextLabel)
 
 	script.Parent.Font = Enum.Font.SourceSans
 end
-coroutine.wrap(JGFVBDT_fake_script)()
-local function MPFAVW_fake_script() -- TextButton_2.LocalScript 
+coroutine.wrap(IILC_fake_script)()
+local function DHWFGM_fake_script() -- TextButton_2.LocalScript 
 	local script = Instance.new('LocalScript', TextButton_2)
 
 	script.Parent.TextSize = 26
@@ -199,18 +214,26 @@ local function MPFAVW_fake_script() -- TextButton_2.LocalScript
 		end
 	end)
 end
-coroutine.wrap(MPFAVW_fake_script)()
-local function TBJEEQR_fake_script() -- TextBox.LocalScript 
+coroutine.wrap(DHWFGM_fake_script)()
+local function RMUDV_fake_script() -- TextBox.LocalScript 
 	local script = Instance.new('LocalScript', TextBox)
 
 	script.Parent.TextSize = 20
 end
-coroutine.wrap(TBJEEQR_fake_script)()
-local function CZBWUI_fake_script() -- MainFrame.LocalScript 
+coroutine.wrap(RMUDV_fake_script)()
+local function YMIUU_fake_script() -- MainFrame.LocalScript 
 	local script = Instance.new('LocalScript', MainFrame)
 
 	script.Parent.Draggable = true
 	script.Parent.Selectable = true
 	
 end
-coroutine.wrap(CZBWUI_fake_script)()
+coroutine.wrap(YMIUU_fake_script)()
+local function FLQUCCX_fake_script() -- TextButton_3.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_3)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Parent:Remove()
+	end)
+end
+coroutine.wrap(FLQUCCX_fake_script)()
